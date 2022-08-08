efafd654eb9bc9b682f613615cb9f2284f04d815
JamesHoi <james352053767@gmail.com>
2022-08-08 23:59:07 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -101,7 +101,10 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/discussions/install-requires-vs-requirements/
-    install_requires=["pystun3"],  # Optional
+    install_requires=[
+        "pystun3"
+        "tqdm"
+    ],  # Optional
 
     tests_require = [
         'pytest',
